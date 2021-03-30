0b945c2c3413a58d9307a1dd438d4eb812682ddf
bryan <bryan.guner@gmail.com>
2021-03-30 14:54:47 -0400

    first session

---
+++ js.js
@@ -0,0 +1,17 @@
+function adder ( a, b ) {
+  let sum = a + b;
+  return sum;
+}
+adder( 1, 2 );
+
+
+
+
+// let adder2 = function ( a, b ) {
+//   
+// }
+// 
+// 
+// let adder3 = () => {
+//   
+// }
