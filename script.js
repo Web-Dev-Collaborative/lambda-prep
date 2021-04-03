3c976ba769b8d3f74deffed05069a38f071a2413
bryan <bryan.guner@gmail.com>
2021-04-03 17:20:11 -0400

    update

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
- let count = 1;
-
+ let state= 1;
+//1 and 0 represent on and off which in this case... after the initial color of red ...will toggle between blue and lime green!
  document.getElementById( "me" ).addEventListener( "click", function () {
 
 
    function setColor( me, color ) {
      let property = document.getElementById( 'me' );
-     if ( count == 0 ) {
+     if ( state== 0 ) {
        property.style.backgroundColor = "blue"
-       count = 1;
+       state= 1;
      } else {
        property.style.backgroundColor = "#7FFF00"
-       count = 0;
+       state= 0;
      }
    }
    setColor();
